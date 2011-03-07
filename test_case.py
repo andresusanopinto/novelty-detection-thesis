2a3d0dcbe249d40244becf77dbb973b8a5d5dfb9
André Susano Pinto <andresusanopinto@gmail.com>
2011-03-07 15:35:15 +0100

    Added DefinePropertySet.
    Helps to make distributions for a set of independent features.

--- test_case.py
+++ test_case.py
@@ -64,39 +64,53 @@ def DefineProperty(name, values):
     return HistogramDistribution(dist)
   return Distribution
 
-Appearance = DefineProperty('appearance', ['kitchen', 'office', 'corridor'])
-RoomShape  = DefineProperty('room_shape', ['square', 'elongated'])
-RoomSize   = DefineProperty('room_size',  ['small', 'medium', 'large'])
-HasBook    = DefineProperty('found_book', ['yes', 'no'])
-HasMilk    = DefineProperty('found_milk', ['yes', 'no'])
-HasScreen  = DefineProperty('found_screen', ['yes', 'no'])
+def DefineIndependentPropertySet(properties):
+  def Distribution(props):
+    features = []
+    for key in properties:
+      if key in props:
+        features.append(properties[key](props[key]))
+      else:
+        features.append(properties[key]({}))
+    return IndependentFeaturesDistribution(features)
+  return Distribution
+
+
+Room = DefineIndependentPropertySet({
+  'Apperance': DefineProperty('appearance', ['kitchen', 'office', 'corridor']),
+  'RoomShape': DefineProperty('room_shape', ['square', 'elongated']),
+  'RoomSize' : DefineProperty('room_size',  ['small', 'medium', 'large']),
+  'HasBook'  : DefineProperty('found_book', ['yes', 'no']),
+  'HasMilk'  : DefineProperty('found_milk', ['yes', 'no']),
+  'HasScreen': DefineProperty('found_screen', ['yes', 'no'])
+})
 
 
 room_categories = {
-  'kitchen': IndependentFeaturesDistribution([
-                 Appearance({'kitchen': 0.70,    'office': 0.29, 'corridor':0.01}),
-                 RoomShape( {'square':  0.60, 'elongated': 0.4}),
-                 RoomSize(  {'small': 0.30, 'medium': 0.6, 'large': 0.2}),
-                 HasBook(   {'yes': 0.4, 'no': 0.6}),
-                 HasMilk(   {'yes': 0.7, 'no': 0.3}),
-                 HasScreen( {'yes': 0.2, 'no': 0.8})
-             ]),
-  'corridor': IndependentFeaturesDistribution([
-                 Appearance({'kitchen': 0.01,    'office': 0.01, 'corridor':0.9}),
-                 RoomShape( {'square':  0.05, 'elongated': 0.95}),
-                 RoomSize(  {'small': 0.50, 'medium': 0.5, 'large': 0.2}),
-                 HasBook(   {'yes': 0.1, 'no': 0.9}),
-                 HasMilk(   {'yes': 0.05, 'no': 0.95}),
-                 HasScreen( {'yes': 0.05, 'no': 0.95})
-             ]),
-  'office':  IndependentFeaturesDistribution([
-                 Appearance({'kitchen': 0.30,   'office': 0.69, 'corridor':0.01}),
-                 RoomShape( {'square':  0.6, 'elongated': 0.4}),
-                 RoomSize(  {'small': 0.20, 'medium': 0.5, 'large': 0.5}),
-                 HasBook(   {'yes': 0.7, 'no': 0.3}),
-                 HasMilk(   {'yes': 0.2, 'no': 0.8}),
-                 HasScreen( {'yes': 0.89, 'no': 0.11}),
-             ]),
+  'kitchen': Room({
+                 'Appearance': {'kitchen': 0.70,    'office': 0.29, 'corridor':0.01},
+                 'RoomShape' : {'square':  0.60, 'elongated': 0.4},
+                 'RoomSize'  : {'small': 0.30, 'medium': 0.6, 'large': 0.2},
+                 'HasBook'   : {'yes': 0.4, 'no': 0.6},
+                 'HasMilk'   : {'yes': 0.7, 'no': 0.3},
+                 'HasScreen' : {'yes': 0.2, 'no': 0.8}
+             }),
+  'corridor': Room({
+                 'Appearance':{'kitchen': 0.01,    'office': 0.01, 'corridor':0.9},
+                 'RoomShape' :{'square':  0.05, 'elongated': 0.95},
+                 'RoomSize'  :{'small': 0.50, 'medium': 0.5, 'large': 0.2},
+                 'HasBook'   :{'yes': 0.1, 'no': 0.9},
+                 'HasMilk'   :{'yes': 0.05, 'no': 0.95},
+                 'HasScreen' :{'yes': 0.05, 'no': 0.95}
+             }),
+  'office':  Room({
+                 'Appearance':{'kitchen': 0.30,   'office': 0.69, 'corridor':0.01},
+                 'RoomShape' :{'square':  0.6, 'elongated': 0.4},
+                 'RoomSize'  :{'small': 0.20, 'medium': 0.5, 'large': 0.5},
+                 'HasBook'   :{'yes': 0.7, 'no': 0.3},
+                 'HasMilk'   :{'yes': 0.2, 'no': 0.8},
+                 'HasScreen' :{'yes': 0.89, 'no': 0.11},
+             }),
 }
 room_distrib = ClassDistribution(DiscreteDistribution(room_categories.keys()), room_categories)
 test_Distribution(room_distrib, 10000)
