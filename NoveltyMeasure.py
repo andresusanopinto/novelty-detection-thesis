24299e268ceec88ba810e6ddaeb067b5daf74a8f
André Susano Pinto <andresusanopinto@gmail.com>
2011-03-10 16:31:56 +0100

    Work on novelty detection based on:
    P( m1 = m2 | sample1 is from m1, sample2 is from m2).
    All models are considered equally likely.

--- NoveltyMeasure.py
+++ NoveltyMeasure.py
@@ -34,18 +34,27 @@ def sample(m, n):
     yield next(m)
 
 def count(data):
-  [positive, negative, total] = sum(map(lambda x: np.array([x == True, x == False, 1]), data))
+  [positive, negative, total] = sum(map(lambda x: np.array([x == True, x == False, 1]), data), np.array([0, 0, 0]))
   return [positive, negative, total]
 
-def beta(a, b):
-  return special.beta(a, b)
 
 dp_fac = {}
 def fac(n):
   return math.factorial(n)
-  if n in dp_fac: return dp_fac[n]
-  dp_fac[n] = math.factorial(n)
-  return dp_fac[n]
+
+def beta(a, b):
+  return special.beta(a, b)
+
+dp_log_fac = {}
+dp_log_fac[0] = math.log(1)
+for i in range(1, 10000):
+  dp_log_fac[i] = dp_log_fac[i-1] + math.log(i)
+
+def log_fac(n):
+  return dp_log_fac[n]
+
+def log_beta(a, b):
+  return log_fac(a-1)+log_fac(b-1)-log_fac(a+b-1)
 
 def comb(n, k):
   return fac(n)/fac(k)/fac(n-k);
@@ -53,19 +62,30 @@ def comb(n, k):
 def train(traindata):
   [t_pos, t_neg, t_size] = count(traindata)
   factor = comb(t_size, t_pos)
-  def novelty(sample):
-    """Measure probability the sample cames from the same distirbution as the train data."""
-    [s_pos, s_neg, s_size] = count(sample)
-    return np.float64(factor)*np.float64(comb(s_size, s_pos))*beta(t_pos+s_pos+1, (t_size-t_pos)+(s_size-s_pos)+1)
   
-  def log_novelty(sample):
+  def novelty2(sample):
+    """This one has normalization factors on it."""
     [s_pos, s_neg, s_size] = count(sample)
-#print("%d-%d - %d-%d" % (t_pos, t_neg, s_pos, s_neg))
-    fac = math.factorial
-    num = fac(t_size)+fac(s_size)+fac(t_pos+s_pos)+fac(t_neg+s_neg)
-    den = fac(t_pos)+fac(t_neg)+fac(s_pos)+fac(s_neg)+fac(t_size+s_size)
-    return num-den
-  return novelty
+    return beta(t_pos+s_pos+1, t_neg+s_neg+1)/beta(t_pos+1, t_neg+1)/beta(s_pos+1, s_neg+1)
+
+  def log_novelty2(sample):
+    [s_pos, s_neg, s_size] = count(sample)
+    return log_beta(t_pos+s_pos+1, t_neg+s_neg+1) - log_beta(t_pos+1, t_neg+1) -log_beta(s_pos+1, s_neg+1)
+
+  def novelty3(sample):
+    return math.exp(log_novelty2(sample))
+  
+  def deb_novelty(sample):
+    sample = list(sample)
+    [s_pos, s_neg, s_size] = count(sample)
+    novel2 = novelty2(sample)
+    novel3 = novelty3(sample)
+    diff = abs(novel3-novel2)
+    if diff > 0.000001:
+      print diff
+    return novel3
+
+  return novelty3
 
 def plot_model_prob(samples, realmodel=None, quality=1000.0):
   pl.xlabel('Model parameter (m)')
@@ -99,7 +119,6 @@ def plot_model_prob(samples, realmodel=None, quality=1000.0):
 
   pl.plot(x, ya, color='red', label='Sum of the %d samples' % len(samples))
   pl.legend(loc = "upper left")
-  pl.show()
   
 
 def test():
@@ -118,7 +137,22 @@ def test1(rmodel=0.75, samples=5, samplesize=5):
   traindata = [list(sample(model(rmodel), samplesize)) for x in range(samples)]
   plot_model_prob(traindata, realmodel=rmodel)
 
-def test2(rmodel=0.95, traindata=None, trainsamples=500, testsamplesize=50, quality=50.0, precision=100):
+def test1_1(rmodel=0.75, numsamples=1, samplesize=10, quality=100.0):
+  """Plots P(m | x)."""
+  for s in range(numsamples):
+    [sp, sn, ss] = count(list(sample(model(rmodel), samplesize)))
+    x = np.arange(0, 1+1/quality, 1/quality)
+    y = x*0
+    denom = beta(sp+1, sn+1)
+    for i in range(quality): y[i] = (x[i]**sp)*((1.0-x[i])**sn)/denom
+    pl.plot(y, label="P(m | Sp=%d, Sn=%d) | (Area = %f)" % (sp, sn, sum(y)/quality))
+
+  pl.title("Model probability given sample.")
+  pl.legend()
+
+
+def test2(rmodel=0.95, traindata=None, trainsamples=500, testsamplesize=50, quality=50.0, precision=100,
+          percentils = [0.05, 0.25, 0.5, 0.75, 0.95]):
   """Plots probability that a sample from a given model is considered to come from the
   same model as a given training data."""
   if traindata == None: traindata = sample(model(rmodel), trainsamples)
@@ -129,26 +163,40 @@ def test2(rmodel=0.95, traindata=None, trainsamples=500, testsamplesize=50, qual
   for (i, m) in enumerate(x):
     md = model(m)
     boxdata[i] = list(psm( sample(md, testsamplesize) ) for x in range(precision))
-    nv = sum(boxdata[i])/precision
+    nv = sum(boxdata[i])/len(boxdata[i])
     y[i] = nv
     print(m,nv)
-  print("Area = %f", sum(y)/quality)
+  print("Area = %f" % (sum(y)/quality))
   
   pl.xlabel('Model parameter (m)')
-  pl.ylabel('P(traindata,sample from m of size %d)' % testsamplesize)
+  pl.ylabel('P(m1=m2 | Train, Sample)' )
   pl.title('Probability a sample from a given model comes from the same model as the training data.'
            ' Training data is a sample of size=%d from m=%f' % (trainsamples, rmodel))
   pl.xlim([-0.05,1.05])
   for i in range(len(x)):
     boxdata[i] = sorted(boxdata[i])
-  for perc in [0.05, 0.25, 0.5, 0.75, 0.95]:
-    p5  = pl.plot(x, list(x[int(perc*precision)] for x in boxdata), label="Percentil %f"%perc)
-#p5 = pl.plot(x, boxdata[:][int(precision*0.05)])
-  pl.plot(x, y)
+  plot_perc = []
+  for perc in percentils:
+    plot_perc.append(list(x[int(perc*precision)] for x in boxdata)) 
+    pl.plot(x, plot_perc[-1], label="Percentil %f"%perc)
+  for i in range(0, len(plot_perc)-1):
+    pl.fill_between(x, plot_perc[i], plot_perc[i+1], alpha=0.25)
+  
+  pl.plot(x, y, label="Expected P(m1=m2|T=train,S=sample(m=m,s=%d))"%testsamplesize)
   pl.legend()
 
-  pl.show()
-    
 
+random.seed(0)    
+if False:
+  pl.figure()
+  test1()
 
-test2()
+if False:
+  pl.figure()
+  test1_1()
+
+if True:
+  pl.figure()
+  test2(rmodel=0.25,trainsamples=500, testsamplesize=500,percentils=[0.05, 0.95])
+
+pl.show()
