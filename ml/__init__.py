4bded1c7c4d132cf1ef8f46da52dc3c9e2b08d93
André Susano Pinto <andresusanopinto@gmail.com>
2011-04-09 18:50:29 +0200

    Added code that can be used to perform simple probabilities estimations.
    Code was moved form the thesis repository.

--- ml/__init__.py
+++ ml/__init__.py
@@ -20,14 +20,49 @@
 #
 from Classifier import *
 from util import Histogram
+import collections
+import itertools
 
-def DiscreteProbabilityEstimator(samples, state_size = 0):
-  counter = Histogram(samples)
-  print counter
-  total = state_size + sum(map(lambda x: x[1], counter.items()), 0)
-  def Density(sample):
-    if sample in counter:
-      return (counter[sample]+1.0) / total 
-    else:
-      return 1.0 / total
-  return Density
+def NormalizedHistogram(data):
+  count_total = 0
+  count_value = collections.defaultdict(int)
+  for sample in data:
+    count_value[sample] += 1
+    count_total += 1
+  
+  def prob(sample):
+    return count_value[sample] / float(count_total)
+  
+  return prob
+
+def ClassDependentEstimator(data, class_estimator, classifier = lambda x: (x[0], x[1])):
+  class_data = collections.defaultdict(list)
+  class_labels = []
+  for sample in data:
+    cname, cdata = classifier(sample)
+    class_data[cname].append(cdata)
+    class_labels.append(cname)
+  class_prob = NormalizedHistogram(class_labels)
+  class_estimator = dict([(cname, class_estimator(cdata)) for cname, cdata in class_data.items()])
+  
+  def prob(sample):
+    p = 0
+    for cname, cestimator in class_estimator.items():
+      p += class_prob(cname)*cestimator(sample)
+    return p
+  return prob
+
+def IndependentFeatureEstimator(data, feature_estimator, feature_iter = iter):
+  feature_data = collections.defaultdict(list)
+  for sample in data:
+    for feature_id, feature_descriptor in feature_iter(sample):
+      feature_data[feature_id].append(feature_descriptor)
+  feature_prob = dict([(fid, feature_estimator(fdata)) for fid,fdata in feature_data.items()])
+  
+  def prob(sample):
+    p = 1.0
+    for fid, fsample in feature_iter(sample):
+      p *= feature_prob[fid](fsample)
+    return p
+  
+  return prob
