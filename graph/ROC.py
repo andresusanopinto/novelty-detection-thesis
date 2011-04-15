0bdd62040cb0160f1e8ebc639fe2117222c84ca3
André Susano Pinto <andresusanopinto@gmail.com>
2011-04-15 15:05:57 +0200

    Change the way roc curves are plotted.
    Now decisions are grouped by threshold so that no longer its made out of "vertical/horizontal" lines.

--- graph/ROC.py
+++ graph/ROC.py
@@ -8,19 +8,18 @@ import pylab as P
 def ROC(data, style = 'ro-', label = None):
   """Plots a ROC curve."""
   
-  def acc(data, value):
-    sum = 0
+  def acc(data):
+    sum = 0.0
     for v in data:
-      if v == value:
-        sum += float(1)
+      sum += v
       yield sum
   
-  fpr = N.array(list(acc(data, False)))
-  tpr = N.array(list(acc(data, True)))
+  fpr = N.array(list(acc(map(lambda x:x[0], data))))
+  tpr = N.array(list(acc(map(lambda x:x[1], data))))
   if fpr[-1] != 0: fpr /= fpr[-1]
   if tpr[-1] != 0: tpr /= tpr[-1]
   
-  P.plot(fpr, tpr, style, markevery=len(fpr)/10, label = label)
+  P.plot(fpr, tpr, style, markevery=(len(fpr)/10, len(fpr)/10), label = label)
   P.xlim([-0.05,1.05])
   P.ylim([-0.05,1.05])
   P.xlabel('False Positive Rate')
