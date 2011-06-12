3f2dfbcb2a068284a7cc6c7e57bf7fb4a4ee885d
André Susano Pinto <andresusanopinto@gmail.com>
2011-06-12 03:07:45 +0200

    Started sample code for thesis.

---
+++ thesis/synthetic/Graph.cc
@@ -0,0 +1,203 @@
+#include <boost/foreach.hpp>
+#include <cstdio>
+#include "Graph-internal.h"
+using namespace std;
+
+#define ROCS_DEBUG_STRINGIFY(x) #x
+#define ROCS_DEBUG_TO_STRING(x) ROCS_DEBUG_STRINGIFY(x)
+
+#define ROCS_DEBUG(...) { \
+  printf("[" __FILE__ ":" ROCS_DEBUG_TO_STRING(__LINE__) "] " __VA_ARGS__); \
+  printf("\n"); \
+}
+
+#define rocsDebug2(...) ROCS_DEBUG(__VA_ARGS__)
+#define rocsDebug1(...) ROCS_DEBUG(__VA_ARGS__)
+
+namespace {
+
+// Unfortunaly is_sorted is a SGI extension.
+// Therefore we need to define it.
+template<typename T>
+bool is_sorted(T begin, T end)
+{
+	if (begin == end)
+		return true;
+
+	T next = begin;
+	for (++next; next != end; begin = next, ++next)
+		if(*next < *begin)
+			return false;
+	return true;
+}
+
+// Builds a vector with the variable types of the given FactorData.
+// Returns false in case not all variables types are defined.
+bool GetVariableTypes(const FactorData &data,
+                      const map<string, VariableType> &known_types,
+                      vector<const VariableType*> *vartypes)
+{
+	typedef pair<string, string> PSS;
+	BOOST_FOREACH(const PSS &v, data.variables)
+	{
+		map<string, VariableType>::const_iterator it = known_types.find(v.second);
+		if (it == known_types.end())
+		{
+			rocsDebug2("Invalid FactorData: Unknown variable type '%s'",
+					v.second.c_str());
+			return false;
+		}
+		vartypes->push_back(&it->second);
+	}
+	return true;
+}
+}  // end namespace anonymous
+
+FactorData* GraphInformation::Add(const string &name, const FactorData &factor)
+{
+	rocsDebug2("Add factor type: '%s'", name.c_str());
+	if (factors.insert(make_pair(name, factor)).second == false)
+		rocsDebug1("Dropped factor: there is already a factor with that name '%s'",
+				name.c_str());
+    return &factors[name];
+}
+
+VariableType* GraphInformation::Add(const string &name, const VariableType &type)
+{
+	rocsDebug2("Add variable type: '%s'", name.c_str());
+	if (types.insert(make_pair(name, type)).second == false)
+		rocsDebug1("Dropped type: there is already a type with that name '%s'",
+				name.c_str());
+    return &types[name];
+}
+
+bool GraphInformation::CheckVarTypeConsistency(const string &name,
+                                               const VariableType &type) const
+{
+	bool consistent = true;
+	if (type.values.size() == 0)
+	{
+		rocsDebug2("VariableType '%s' has no defined possible values.", name.c_str());
+		consistent = false;
+	}
+
+	if (!is_sorted(type.values.begin(), type.values.end()))
+	{
+		rocsDebug2("VariableType '%s' values are not sorted.", name.c_str());
+		consistent = false;
+	}
+	return consistent;
+}
+
+bool GraphInformation::CheckFactorConsistency(const string &name,
+                                              const FactorData &factor) const
+{
+	if (factor.variables.size() == 0)
+	{
+		rocsDebug2("FactorData '%s' does not has any variable.", name.c_str());
+		return false;
+	}
+
+	vector<const VariableType*> vartypes;
+	if (!GetVariableTypes(factor, types, &vartypes))
+	{
+		rocsDebug2("FactorData '%s' has undefined variable types.", name.c_str());
+		return false;
+	}
+
+	// Variables types are correct, check potentials consistency
+	bool consistent = true;
+
+	typedef pair<vector<string>, double> PotentialIt;
+	BOOST_FOREACH(const PotentialIt &p, factor.potential)
+	{
+		// Non-negative potentials only.
+		if (p.second < 0)
+		{
+			rocsDebug2("FactorData '%s': found negative potential.", name.c_str());
+			consistent = false;
+		}
+
+		// Check potential index.
+		if (p.first.size() != vartypes.size())
+		{
+			rocsDebug2("FactorData '%s': found potential that does not agrees in"
+			           " number of variables", name.c_str());
+			consistent = false;
+			continue;
+		}
+
+		for (size_t i = 0; i != vartypes.size(); ++i)
+		{
+			// Considering we have the indexes sorted and the variables sorted this
+			// is inefficient. Though it does not matters since this code is mainly
+			// for helping debugging.
+			if (find(vartypes[i]->values.begin(), vartypes[i]->values.end(),
+					p.first[i]) == vartypes[i]->values.end())
+			{
+				rocsDebug2("FactorData '%s': potential with incorrect value"
+						"'%s' for variable %s.", name.c_str(),
+						p.first[i].c_str(), factor.variables[i].first.c_str());
+				consistent = false;
+			}
+		}
+	}
+
+	// Check if all potentials were defined.
+	size_t total_size = 1;
+	BOOST_FOREACH(const VariableType *type, vartypes)
+		total_size *= type->values.size();
+
+	if (total_size != factor.potential.size())
+	{
+		rocsDebug2("FactorData '%s': not all potentials are defined.", name.c_str());
+		consistent = false;
+	}
+
+	return consistent;
+}
+
+bool GraphInformation::CheckConsistency() const
+{
+	bool consistent = true;
+
+	// Check VariableType consistency
+	typedef pair<string, VariableType> VarTypeIt;
+	BOOST_FOREACH(const VarTypeIt &vartype, types)
+		consistent &= CheckVarTypeConsistency(vartype.first, vartype.second);
+
+	// Check FactorData consistency
+	typedef pair<string, FactorData> FactorDataIt;
+	BOOST_FOREACH(const FactorDataIt &factor, factors)
+		consistent &= CheckFactorConsistency(factor.first, factor.second);
+
+	return consistent;
+}
+
+Graph::Graph()
+{}
+
+Graph::~Graph()
+{}
+
+const Variable* Graph::CreateVariable(const VariableType *type)
+{
+	Variable *variable = new Variable(type);
+	variables_.push_back(variable);
+	return variable;
+}
+
+const Factor* Graph::CreateFactor(const FactorData *data,
+                           const vector<const Variable*> &variables)
+{
+    rocsDebug2("Creating a factor of type %x", data);
+	Factor* factor = new Factor(data, variables);
+	factors_.push_back(factor);
+	return factor;
+}
+
+void Graph::ListFactors(vector<const Factor*> *output) const {
+	output->reserve(output->size() + factors_.size());
+	BOOST_FOREACH(const Factor &f, factors_)
+	  output->push_back(&f);
+}
---
+++ thesis/synthetic/main.cc
@@ -0,0 +1,187 @@
+/**
+ * Novelty Detection Scripts
+ *
+ * Copyright 2011: André Susano Pinto
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#include "Graph.h"
+#include "Query.h"
+#include "util.h"
+
+#include <cassert>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+#include <boost/foreach.hpp>
+#include <boost/scoped_ptr.hpp>
+using namespace boost;
+
+#include <dai/factorgraph.h>
+#include <dai/gibbs.h>
+#include <dai/properties.h>
+using namespace dai;
+
+GraphInformation gi;
+const VariableType* roomType;
+const VariableType* senseType;
+const FactorData* room_room;
+const FactorData* room_sense;
+
+const string ROOM_TYPE  = "ROOM";
+const string SENSE_TYPE = "SENSE";
+
+
+string room(int i)  { return "room" + to_string(i); }
+string sense(int i) { return "sense" + to_string(i); }
+
+// Initializes GI with a random real distribution.
+void gi_init_random_real(int roomCats, int senseCats) {
+  // Generate states for variables of type Room
+  {
+    VariableType roomCat;
+    for (int i = 0; i < roomCats; ++i)
+      roomCat.values.push_back(room(i));
+    sort(roomCat.values.begin(), roomCat.values.end());
+    roomType = gi.Add(ROOM_TYPE, roomCat);
+  }
+
+  // Generate states for sensed properties
+  {
+    VariableType senseCat;
+    for (int i = 0; i < senseCats; ++i)
+      senseCat.values.push_back(sense(i));
+    sort(senseCat.values.begin(), senseCat.values.end());
+    senseType = gi.Add(SENSE_TYPE, senseCat);
+  }
+
+  // Generate factors between Room Categories
+  {
+    // This factor is symmetric.
+    // This is the approach that is taken on the room connectivity
+    // otherwise a scheme to figure out on which direction to plug
+    // the factor would need to be devised.
+    FactorData rr;
+    rr.variables.push_back(make_pair(string("room1"), ROOM_TYPE));
+    rr.variables.push_back(make_pair(string("room2"), ROOM_TYPE));
+    vector<string> index(2);
+    for (int i = 0; i < roomCats; ++i)
+    for (int j = i; j < roomCats; ++j) {
+      double potential = random();
+      index[0] = room(i);
+      index[1] = room(j);
+      rr.potential[index] = potential;
+      swap(index[0], index[1]);
+      rr.potential[index] = potential;
+    }
+    room_room = gi.Add("room-room", rr);
+  }
+
+  // Generate factors for Room Category and Sensed Property
+  {
+    FactorData rs;
+    rs.variables.push_back(make_pair(string("room"), ROOM_TYPE));
+    rs.variables.push_back(make_pair(string("sense"), SENSE_TYPE));
+    vector<string> index(2);
+    for (int i = 0; i < roomCats; ++i)
+    for (int j = 0; j < senseCats; ++j) {
+      index[0] = room(i);
+      index[1] = sense(j);
+      rs.potential[index] = random();
+    }
+    room_sense = gi.Add("room-sense", rs);
+  }
+}
+
+struct GraphStructure {
+  int rooms;
+  int senses;
+  set<pair<int,int> > edges;
+
+  void createRandom(int rooms, int senses, int extra_conn) {
+    this->rooms = rooms;
+    this->senses = senses;
+    // Creates a tree
+    for (int i = 1; i < rooms; ++i) {
+      int other = random() % i;
+      edges.insert(make_pair(other, i));
+    }
+    // Tries to add extra room connectivity
+    while (extra_conn--) {
+      int a = random() % rooms;
+      int b = random() % rooms;
+      if (a == b) continue;
+      if (a > b) swap(a, b);
+      edges.insert(make_pair(a, b));
+    }
+    // Adds senses (they are randomly split by the rooms)
+    for (int i = 0; i < senses; ++i) {
+      int r = random() % rooms;
+      edges.insert(make_pair(r, rooms+i));
+    }
+  }
+};
+
+class MGraph : public Graph {
+
+public:
+  vector<const Variable*> vars;
+
+  MGraph(const GraphStructure &structure,
+         const VariableType *room,
+         const VariableType *sense,
+         const FactorData *rrf,
+         const FactorData *rsf) {
+    // Build Graph
+    for (int i = 0; i < structure.rooms; ++i)
+      vars.push_back(CreateVariable(room));
+    for (int i = 0; i < structure.senses; ++i)
+      vars.push_back(CreateVariable(sense));
+  
+    typedef pair<int,int> Edge;
+    BOOST_FOREACH(const Edge &e, structure.edges) {
+      int a = e.first;
+      int b = e.second;
+      vector<const Variable*> nodes;
+      nodes.push_back(vars[a]);
+      nodes.push_back(vars[b]);
+      CreateFactor(b < structure.rooms? rrf : rsf, nodes);
+    }
+  }
+
+};
+
+
+
+
+int main() {
+  gi_init_random_real(3, 6);
+  assert(gi.CheckConsistency());
+
+  GraphStructure s;
+  s.createRandom(4, 4, 1);
+  MGraph g(s, roomType, senseType, room_room, room_sense);
+
+  Query q(&g);
+  vector<string> output;
+  q.Sample(g.vars, &output);
+
+  BOOST_FOREACH(const string &s, output)
+    cout << s << endl;
+
+  return 0;
+}
+
